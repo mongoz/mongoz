a9ce6cd81aa8afc1324f8a8e10adb127e7a7fbc0
mongoz <makarivanov17y@gmail.com>
2021-10-23 19:04:32 +0300

    1.0

---
+++ topic5_function/17.even_odd_counter.py
@@ -0,0 +1,26 @@
+"""Счетчик четных/нечетных чисел. В этой главе вы увидели пример написания алгоритма,
+который определяет четность или нечетность числа. Напишите программу, которая
+генерирует 100 случайных чисел и подсчитывает количество четных и нечетных
+случайных чисел"""
+
+import random
+
+
+def main():
+    total_even_number = 0
+    total_odd_number = 0
+    for num in range(1, 101):
+        number = generate_randomN()
+        if (number % 2) == 0:
+            total_even_number += 1
+        else:
+            total_odd_number += 1
+    print(f'Общее к-во чётных чисел = {total_even_number}\n'
+          f'Общее к-во нечётных чисел = {total_odd_number} ')
+
+
+def generate_randomN():
+    number = random.randint(1, 1000)
+    return number
+
+main()
