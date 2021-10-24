4e70731957310c88b63bc744cedcd19e229e6a75
mongoz <makarivanov17y@gmail.com>
2021-10-24 16:17:56 +0300

    1.0

--- topic5_function/18.prime_numbers.py
+++ topic5_function/18.prime_numbers.py
@@ -2,13 +2,13 @@ def main():
     print('Программа проверяет простое число, либо сложное.\n')
     user_number = int(input('Введите число для проверки: '))
     status = is_prime(user_number)
-    if user_number == 1:
-        print('1 - не простое число.')
-    else:
+    if user_number != 1:
         if is_prime(user_number):
             print(status, '(это простое число)')
         else:
             print(status, '(это сложное число)')
+    else:
+        print("1 - не простое число.")
 
 
 def is_prime(user_number):
