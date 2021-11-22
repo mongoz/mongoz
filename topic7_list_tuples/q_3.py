220467664e31b554ed00ad692dea45d99d54b3a3
mongoz <makarivanov17y@gmail.com>
2021-11-22 14:39:56 +0200

    1.0.1

---
+++ topic7_list_tuples/q_3.py
@@ -0,0 +1,32 @@
+def main():
+    months = ['Январь', 'Февраль', 'Март',
+              'Апрель', 'Май', 'Июнь', 'Июль',
+              'Август', 'Сентябрь', 'Октябрь',
+              'Ноябрь', 'Декабрь']
+    # print(months)
+    list_mod(months)
+
+
+def list_mod(months):
+    list_values = []
+    total = 0
+    m = 0  # месяц
+    while m <= len(months) - 1:
+        user_input = float(input(f'Введите к-о осадков за {months[m]}: '))
+        list_values.append(user_input)
+        total += user_input
+        m += 1
+    # print(list_values)
+    average = total / len(months)
+    min_month = months[list_values.index(min(list_values))]
+    max_month = months[list_values.index(max(list_values))]
+
+    min_value = min(list_values)
+    max_value = max(list_values)
+    print(f"Total rainfall this year = {total:.2f}\n"
+          f"Average rainfall this year = {average:.2f}\n"
+          f"The month with the highest rainfall this year - {max_month} = {max_value:.2f}\n"
+          f"The month with the lowest rainfall this year - {min_month} = {min_value:.2f}")
+
+
+main()
